5784121c825b0bf5bf44803628fb5702101b5349
gregorbj <gregorbj@gmail.com>
2019-05-21 10:41:01 -0700

    Implement project folder selection ui and making model selection elements conditional on the selection of a project folder

--- inst/logic-lab/ui.R
+++ inst/logic-lab/ui.R
@@ -43,7 +43,7 @@ textareaInput <-
 shinyUI(
   navbarPage(
     "Logic Laboratory",
-    
+
     #Introduction Screen
     #-------------------
     tabPanel(
@@ -81,34 +81,34 @@ shinyUI(
           strong("Proximity"),
           " concept is shown as affecting the ",
           strong("Auto Trip Distance"),
-          " concept. Edge weights specify the strength and directionality of 
-          causal effects. A positive sign for an edge weight means that an 
-          increase in the causing concept causes an increase in the affected 
+          " concept. Edge weights specify the strength and directionality of
+          causal effects. A positive sign for an edge weight means that an
+          increase in the causing concept causes an increase in the affected
           concept. In the example, an increase in the ",
           strong("Auto Trip Distance"),
           " concept causes a decrease in the ",
           strong("Auto Trip Rate"),
           " concept and an increase in the ",
           strong("VMT"),
-          " concept. In the FSDM, as in the FCM, the strength of causal effects 
-          is expressed in ‘fuzzy’ terms. In the example, the weights are L 
-          (low), M (moderate), and H (high), as opposed to specific numeric 
+          " concept. In the FSDM, as in the FCM, the strength of causal effects
+          is expressed in ‘fuzzy’ terms. In the example, the weights are L
+          (low), M (moderate), and H (high), as opposed to specific numeric
           values."
         ),
         p(
-          "At its core, FSDM involves defining the concepts and edges. Each 
-          concept has a name, a description, and a range of possible values. 
-          Each relation defined by a causal concept and an affected concept, the 
-          sign of the relationship (+ or -), and the strength of the relationship. 
-          One or many scenarios may be run on a FSDM. Each scenario is defined by 
-          the starting values assigned to every concept and by the final values 
-          assigned to one or more of the concepts. When the FSDM is run, the 
-          concepts that have assigned final values have their values incremented 
-          in small steps between the starting and final values. With each 
-          increment, the orders of causal effects are calculated until every 
+          "At its core, FSDM involves defining the concepts and edges. Each
+          concept has a name, a description, and a range of possible values.
+          Each relation defined by a causal concept and an affected concept, the
+          sign of the relationship (+ or -), and the strength of the relationship.
+          One or many scenarios may be run on a FSDM. Each scenario is defined by
+          the starting values assigned to every concept and by the final values
+          assigned to one or more of the concepts. When the FSDM is run, the
+          concepts that have assigned final values have their values incremented
+          in small steps between the starting and final values. With each
+          increment, the orders of causal effects are calculated until every
           causal effect is accounted for. For example, if the ",
           strong("Proximity"),
-          " concept in the example FSDM above is being incremented, then with 
+          " concept in the example FSDM above is being incremented, then with
           each increment the following are calculated:"
         ),
         tags$ol(
@@ -137,82 +137,82 @@ shinyUI(
           )
         ),
         p(
-          "If the FSDM includes one or more cycles (i.e. feedback loops), these 
-          calculations are repeated until the changes in concept values with 
-          each repeated set of calculations is very small or until a specified 
+          "If the FSDM includes one or more cycles (i.e. feedback loops), these
+          calculations are repeated until the changes in concept values with
+          each repeated set of calculations is very small or until a specified
           maximum number of repeat calculations is reached."
         ),
         hr(),
         h3("How to Build and Run a FSDM", style = "color: blue"),
         p(
-          "The Logic Laboratory interface will guide you through the process of 
-          1) building a FSDM model, 
-          2) creating scenarios to model with the FSDM, 
-          3) running the FSDM on the scenarios, and 
-          4) analyzing the model results. 
-          The four numbered 'tabs' at the top of the page correspond to these 
-          tasks. Clicking on a 'tab' changes the interface to guide you through 
-          the process of carrying out the task. Following are summaries of what 
+          "The Logic Laboratory interface will guide you through the process of
+          1) building a FSDM model,
+          2) creating scenarios to model with the FSDM,
+          3) running the FSDM on the scenarios, and
+          4) analyzing the model results.
+          The four numbered 'tabs' at the top of the page correspond to these
+          tasks. Clicking on a 'tab' changes the interface to guide you through
+          the process of carrying out the task. Following are summaries of what
           is done in each task:"
         ),
         tags$ol(
           tags$li(
             strong("Build a Model:"),
-            " Building a FSDM model involves five steps that are listed as menu 
-            items: User Info, Edit Concepts, Edit Relations, Save Model. The 
-            first step, entering user information is necessary in order to 
-            attribute models and model edits. The second step, selecting a model, 
-            shows a view which enables you to start a model from scratch, copy 
-            an existing model to serve as the starting point for your model, 
-            edit an existing model, or run an existing model without editing. 
-            The third step, editing concepts, is where you define new concepts 
-            or edit existing concepts and the range of values that they may 
-            have. The fourth step, editing relations, is where you define the 
-            relationships between concepts (magnitude and direction). The final 
-            step, saving the model, permanently saves all of the model edits 
+            " Building a FSDM model involves five steps that are listed as menu
+            items: User Info, Edit Concepts, Edit Relations, Save Model. The
+            first step, entering user information is necessary in order to
+            attribute models and model edits. The second step, selecting a model,
+            shows a view which enables you to start a model from scratch, copy
+            an existing model to serve as the starting point for your model,
+            edit an existing model, or run an existing model without editing.
+            The third step, editing concepts, is where you define new concepts
+            or edit existing concepts and the range of values that they may
+            have. The fourth step, editing relations, is where you define the
+            relationships between concepts (magnitude and direction). The final
+            step, saving the model, permanently saves all of the model edits
             along with notes to document the model or edits to the model."
           ),
           tags$li(
             strong("Create Scenarios:"),
-            " One or more model scenarios can be created for a FSDM. This 'tab' 
-            of the application assists you with creating single or multiple 
-            scenarios, and with editing existing scenarios. A new scenario can 
-            be started from scratch or created from an existing scenario. 
-            Scenarios are validated against the defined model to assure that 
+            " One or more model scenarios can be created for a FSDM. This 'tab'
+            of the application assists you with creating single or multiple
+            scenarios, and with editing existing scenarios. A new scenario can
+            be started from scratch or created from an existing scenario.
+            Scenarios are validated against the defined model to assure that
             scenario values are consistent with defined concept value ranges."
           ),
           tags$li(
             strong("Run the Model:"),
-            " The FSDM may be run for one or more of the scenarios. This 'tab' 
-            enables you to specify which of the scenarios to run. The interface 
-            shows a checkbox list of all of the scenarios that have been created 
-            for a model. Entering a check in a checkbox marks the corresponding 
-            scenario to be run. A button starts the model runs. Progress of the 
+            " The FSDM may be run for one or more of the scenarios. This 'tab'
+            enables you to specify which of the scenarios to run. The interface
+            shows a checkbox list of all of the scenarios that have been created
+            for a model. Entering a check in a checkbox marks the corresponding
+            scenario to be run. A button starts the model runs. Progress of the
             model run is shown as the model run proceeds."
           ),
           tags$li(
             strong("Analyze Results:"),
-            " This 'tab' of the application enables users to view the results 
-            for one or two scenarios. The user can choose which model scenarios 
-            and which concept values to display. The results are displayed in 
-            graphs. Users can choose to save the displayed graphs and 
+            " This 'tab' of the application enables users to view the results
+            for one or two scenarios. The user can choose which model scenarios
+            and which concept values to display. The results are displayed in
+            graphs. Users can choose to save the displayed graphs and
             corresponding data."
           )
         ),
         hr(),
         h3("Copyright and License", style = "color: blue"),
         p(
-          "The Logic Laboratory was developed by Brian Gregor (Oregon Systems 
-          Analytics) with funding from the Oregon Department of Transportation. 
-          It is licensed with the Apache 2 open source license. Terms of the 
-          license are included in the LICENSE text file. Additional notices are 
+          "The Logic Laboratory was developed by Brian Gregor (Oregon Systems
+          Analytics) with funding from the Oregon Department of Transportation.
+          It is licensed with the Apache 2 open source license. Terms of the
+          license are included in the LICENSE text file. Additional notices are
           included in the NOTICE text file."
         ),
         h4("Copyright 2016 Oregon Department of Transportation"),
         h4("License Apache 2")
       )
     ),
-    
+
     #Build a Model Screen
     #--------------------
     navbarMenu(
@@ -223,7 +223,7 @@ shinyUI(
                    h4("User Information"),
                    hr(),
                    p(
-                     "The user information entered below is used to attribute 
+                     "The user information entered below is used to attribute
                      model creation and editing."
                    ),
                    textInput("firstName", "First Name"),
@@ -239,32 +239,34 @@ shinyUI(
                    shinyDirButton("ProjectFolder", "Folder select", "Please select a folder"),
                    hr(),
                    h4("Select Model"),
-                   hr(),
-                   radioButtons(
-                     inputId = "modelAction",
-                     label = "Model Action",
-                     choices = list(
-                       "Create New Model From Scratch" = "newModel",
-                       "Create New Model From Copy" = "copyModel",
-                       "Edit Existing Model" = "editModel",
-                       "Run Existing Model Without Editing" = "runModel"
-                     )
-                   ),
-                   conditionalPanel(condition = "input.modelAction == 'newModel' || 
-                                    input.modelAction == 'copyModel'",
-                                    textInput("modelName", "Model Name", "")),
-                   bsAlert("nonameAlert"),
-                   bsAlert("duplicateModel"),
-                   bsAlert("noAuthorInfo"),
-                   conditionalPanel(condition = "input.modelAction == 'copyModel' || 
-                                    input.modelAction == 'editModel' || 
-                                    input.modelAction == 'runModel'",
-                                    uiOutput("selectModelFile")),
                    conditionalPanel(
-                     condition = "input.modelAction == 'copyModel'",
-                     checkboxInput("copyScenarios", "Copy scenarios too?")
-                   ),
-                   actionButton("startModeling", "Start Working on Model")
+                     condition = "output.projectFolder != ''",
+                     radioButtons(
+                       inputId = "modelAction",
+                       label = "Model Action",
+                       choices = list(
+                         "Create New Model From Scratch" = "newModel",
+                         "Create New Model From Copy" = "copyModel",
+                         "Edit Existing Model" = "editModel",
+                         "Run Existing Model Without Editing" = "runModel"
+                       )
+                     ),
+                     conditionalPanel(condition = "input.modelAction == 'newModel' ||
+                                    input.modelAction == 'copyModel'",
+                                      textInput("modelName", "Model Name", "")),
+                     bsAlert("nonameAlert"),
+                     bsAlert("duplicateModel"),
+                     bsAlert("noAuthorInfo"),
+                     conditionalPanel(condition = "input.modelAction == 'copyModel' ||
+                                    input.modelAction == 'editModel' ||
+                                    input.modelAction == 'runModel'",
+                                      uiOutput("selectModelFile")),
+                     conditionalPanel(
+                       condition = "input.modelAction == 'copyModel'",
+                       checkboxInput("copyScenarios", "Copy scenarios too?")
+                     ),
+                     actionButton("startModeling", "Start Working on Model")
+                   )
                  ),
                  mainPanel(
                    h4("Project Folder: ", textOutput("projectFolder", inline = TRUE)),
@@ -387,10 +389,10 @@ shinyUI(
                            p(
                              "Pressing the ",
                              strong("Save Model"),
-                             " button will save the save the edited model, 
-                             overwriting the Concepts and Relations files and 
-                             updating the status file. The notes entered into 
-                             the notes text area will overwrite any notes since 
+                             " button will save the save the edited model,
+                             overwriting the Concepts and Relations files and
+                             updating the status file. The notes entered into
+                             the notes text area will overwrite any notes since
                              the last save in this session."
                            ),
                            textareaInput("modelNotes", "Model Notes"),
@@ -398,7 +400,7 @@ shinyUI(
                          ))
       )
     ),
-    
+
     #Create Scenarios Screen
     #-----------------------
     navbarMenu(
@@ -416,11 +418,11 @@ shinyUI(
                        "Edit Existing Scenario" = "editScenario"
                      )
                    ),
-                   conditionalPanel(condition = "input.scenarioAction == 'newScenario' || 
+                   conditionalPanel(condition = "input.scenarioAction == 'newScenario' ||
                                     input.scenarioAction == 'copyScenario'",
                                     textInput("scenarioName", "Scenario Name", "")),
                    bsAlert("noscenarioAlert"),
-                   conditionalPanel(condition = "input.scenarioAction == 'copyScenario' || 
+                   conditionalPanel(condition = "input.scenarioAction == 'copyScenario' ||
                                     input.scenarioAction == 'editScenario'",
                                     uiOutput("selectScenarioFile")),
                    actionButton("startScenario", "Start Working on Scenario")
@@ -453,7 +455,7 @@ shinyUI(
         )
       )
     ),
-    
+
     #Run the Model Screen
     #--------------------
     tabPanel(
@@ -476,8 +478,8 @@ shinyUI(
           p(
             "Pressing the ",
             strong("Run Model"),
-            " button will run the model for all the scenarios that are checked 
-            in the list above. Outputs will be saved in the respective scenario 
+            " button will run the model for all the scenarios that are checked
+            in the list above. Outputs will be saved in the respective scenario
             directories."
           ),
           actionButton("runModel", "Run Model"),
@@ -487,7 +489,7 @@ shinyUI(
         mainPanel()
       )
     ),
-    
+
     #Analyze Results Screen
     #----------------------
     tabPanel(
@@ -511,6 +513,6 @@ shinyUI(
           )
       )
     )
-    
+
   )
 )
